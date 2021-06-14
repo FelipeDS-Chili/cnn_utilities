82e996559237b368ee247f9f36b6f1c45dd23796
felipe inostroza <falberto.inostroza@gmail.com>
2021-06-14 16:35:56 -0400

    m

--- cnn_utilities/utils.py
+++ cnn_utilities/utils.py
@@ -1,125 +1,51 @@
-import numpy as numpy
-import time
-from sklearn.metrics import recall_score, precision_score, f1_score
+
+import urllib.request
 import os
-import pandas as pd
 
 ######################################## METRICS #########################################################
+def download_images(google_file, folder, root, output_height, output_width):
 
-def compute_precision_macro(y_pred, y_true):
+    ''''
+    google_file: file where it is contained the cURLs text
+    folder: image class folder
+    root: root folder which contains all the images
+    output_height: height wished for the image downloaded
+    output_width: width wished for the image downloaded
 
-    return precision_score(y_true, y_pred, average='macro')
+    '''
+    # Bajar fotos desde el archivo de google
+    photos_urls = list()
+    count = 1
+    with open(google_file) as f:
+        for line in f:
+            start_index = line.find("https://encrypted-")
+            if start_index != -1:
+                end_index = line[start_index:].find("^")
+                photos_urls.append(line[start_index:start_index+end_index])
+            count += 1
 
-def compute_precision(y_pred, y_true):
+    # Preprocesar imagenes
+    try:
+        os.mkdir(root+"/"+folder)
+    except:
+        pass
+    try:
+        os.mkdir(root+"/"+folder+"_prepro")
+    except:
+        pass
+    count = 1
+    for link in photos_urls:
+        urllib.request.urlretrieve(link, root+"/"+folder+"/img"+str(count)+".jpg")
+        count+= 1
 
-
-    return precision_score(y_true, y_pred)
-
-def compute_f1_macro(y_pred, y_true):
-
-
-    return precision_score(y_true, y_pred, average='macro')
-
-
-def compute_recall(y_pred, y_true):
-
-     return recall_score(y_true, y_pred)
-
-def compute_f1(y_pred, y_true):
-
-     return f1_score(y_true, y_pred)
-
-def simple_time_tracker(method):
-    def timed(*args, **kw):
-        ts = time.time()
-        result = method(*args, **kw)
-        te = time.time()
-        if 'log_time' in kw:
-            name = kw.get('log_name', method.__name__.upper())
-            kw['log_time'][name] = int((te - ts))
-        else:
-            print(method.__name__, round(te - ts, 2))
-        return result
-
-    return timed
-
-def compute_rmse(y_pred, y_true):
-    return np.sqrt(((y_pred - y_true) ** 2).mean())
-
-
-
-
-
-######################################## DISTANCE FUNCTIONS #########################################################
-
-
-
-def haversine_vectorized(df,
-                         start_lat="pickup_latitude",
-                         start_lon="pickup_longitude",
-                         end_lat="dropoff_latitude",
-                         end_lon="dropoff_longitude"):
-    """
-        Calculate the great circle distance between two points
-        on the earth (specified in decimal degrees).
-        Vectorized version of the haversine distance for pandas df
-        Computes distance in kms
-        TOMA UN DF CON ESAS COLUMNAS Y DEVUELVE UN ARRAY CON LOS RESULTADOS
-
-    """
-
-    lat_1_rad, lon_1_rad = np.radians(df[start_lat].astype(float)), np.radians(df[start_lon].astype(float))
-    lat_2_rad, lon_2_rad = np.radians(df[end_lat].astype(float)), np.radians(df[end_lon].astype(float))
-    dlon = lon_2_rad - lon_1_rad
-    dlat = lat_2_rad - lat_1_rad
-
-    a = np.sin(dlat / 2.0) ** 2 + np.cos(lat_1_rad) * np.cos(lat_2_rad) * np.sin(dlon / 2.0) ** 2
-    c = 2 * np.arcsin(np.sqrt(a))
-    return 6371 * c
-
-
-def minkowski_distance(df, p,
-                       start_lat="pickup_latitude",
-                       start_lon="pickup_longitude",
-                       end_lat="dropoff_latitude",
-                       end_lon="dropoff_longitude"):
-    x1 = df[start_lon]
-    x2 = df[end_lon]
-    y1 = df[start_lat]
-    y2 = df[end_lat]
-    return ((abs(x2 - x1) ** p) + (abs(y2 - y1)) ** p) ** (1 / p)
-
-
-
-#####################################  OPTIMIZATION INTEGERS AND FLOATS (DOWNCAST) #####################################
-
-
-def df_optimized(df, verbose=False, **kwargs):
-    """
-    Reduces size of dataframe by downcasting numeircal columns
-    :param df: input dataframe
-    :param verbose: print size reduction if set to True
-    :param kwargs:
-    :return: df optimized
-    """
-    in_size = df.memory_usage(index=True).sum()
-    # Optimized size here
-    for type in ["float", "integer"]:
-        l_cols = list(df.select_dtypes(include=type))
-        for col in l_cols:
-            df[col] = pd.to_numeric(df[col], downcast=type)
-            if type == "float":
-                df[col] = pd.to_numeric(df[col], downcast="integer")
-    out_size = df.memory_usage(index=True).sum()
-    ratio = (1 - round(out_size / in_size, 2)) * 100
-    GB = out_size / 1000000000
-    if verbose:
-        print("optimized size by {} % | {} GB".format(ratio, GB))
-    return df
-
-
-
-
+    for filename in os.listdir(root+"/"+folder):
+        try:
+            image = Image.open(root+"/"+folder+"/"+filename)
+            new_image = image.resize((output_width, output_height))
+            new_image.save(root+"/"+folder+"_prepro/"+filename)
+        except:
+            pass
+    return 'Done'
 
 
 
